3d796df5682b9401222760dd3d650c24b4b478c6
Chase <chase.moon93@gmail.com>
2024-07-10 10:48:33 -0700

    The bot is now handling hello world intents

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from properties_utils import get_bot_token
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'NewsBot logged in as {client.user}')
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found.")
+    else:
+        raise error
+
+@client.command()
+async def hello(ctx):
+    print(f'recieved a hello command')
+    await ctx.send('Hello, world!')
+
+client.run(get_bot_token())

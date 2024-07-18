50888a808160e43baaa902590a42d4a8a688b53c
Chase <chase.moon93@gmail.com>
2024-07-18 12:00:12 -0700

    Updated the code that fetches the archive url

--- bot.py
+++ bot.py
@@ -10,13 +10,32 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix='!', intents=intents)
 
+def find_latest_archive_timestamp(url):
+    cleaned_url = urllib.parse.quote(url, safe='')
+    timestamps_url = f'https://web.archive.org/__wb/sparkline?output=json&url={cleaned_url}&collection=web'
+
+    payload = {}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Referer': f'https://web.archive.org/web/*/{url}'
+    }
+
+    response = requests.get(timestamps_url, headers=headers, data=payload)
+    try:
+        j = response.json()
+    except:
+        j = {}
+    return dict(j).get('last_ts', '*') or '*'
+
 @client.event
 async def on_ready():
     print(f'NewsBot logged in as {client.user}')
 
-@client.command()
-async def archive(ctx, url: str):
-    await ctx.send(get_latest_archive_url(url))
+@bot.command(name='paywall', help='Responds a link to Archive.org')
+async def get_archive_link(ctx, url, *args):
+    timestamp = find_latest_archive_timestamp(url)
+    response = f'https://web.archive.org/web/{timestamp}/{url}'
+    await ctx.send(response)
 
 @client.event
 async def on_command_error(ctx, error):
